ddd69c78a0d87e2bc1aa80df2008f8d3e1a7e7d8
MD JAVED ALI <mdjavedali67990@gmail.com>
2026-06-18 00:53:16 +0530

    created medicine controller to direct requests to service

--- backend/controllers/medicine.controller.js
+++ backend/controllers/medicine.controller.js
@@ -1,60 +1,37 @@
-import Medicine from "../models/Medicine.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import * as medicineService from "../services/medicine.service.js"
 
-// 1. GET all medicines
-export const getAllMedicines = (req, res) => {
-    try{
-        const list = Medicine.findAll();
-        res.status(200).json(list);
-    } catch(error) {
-        res.status(500).json({error: error.message});
+// Get all 
+export const getAllMedicines = asyncHandler(
+    async (req, res) => {
+        const medicines = medicineService.getAllMedicines();
+        res.status(200).json({
+            success: true,
+            count: medicines.length,
+            data: medicines
+        });
     }
-};
+);
 
-// 2. POST create new medicine
-export const addMedicines = (req, res) => {
-    try{
-        const {name, batch_number, barcode, category, company, supplier_id, mrp, dr_price, price, quantity, expiry_date} = req.body;
-
-        if(!name || !batch_number || !company || !supplier_id || !mrp || !dr_price || !price || quantity === undefined || !expiry_date) {
-            return res.status(400).json({message: "Please fill required input fields."})
-        }
-
-        const newId = Medicine.create({name, batch_number, barcode, category, company, supplier_id, mrp, dr_price, price, quantity, expiry_date});
-        res.status(201).json({ message: "Medicine added successfully", id: newId });
-    }catch(error) {
-        if(error.message.includes("UNIQUE constraint failed")) {
-            return res.status(400).json({message: "A medicine with this name already exists."});
-        }
-        return res.status(500).json({error: error.message});
+// Get by id
+export const getMedicineById = asyncHandler(
+    async (req, res) => {
+        const medicines = medicineService.getMedicineById(req.params.id);
+        res.status(200).json({
+            success: true,
+            data: medicines
+        });
     }
-};
+);
 
-// 3. PUT update existing medicine
-export const updateMedicines = (req, res) => {
-    try{
-        const {id} = req.params;
-        const success = Medicine.update(id, req.body);
-
-        if(!success) {
-            return res.status(404).json({message: "Medicine record not found."})
-        }
-        res.status(200).json({message: "Inventory updated successfully."})
-    } catch(error) {
-        res.status(500).json({ error: error.message });
+// create 
+export const createMedicine = asyncHandler(
+    async (req, res) => {
+        const medicine = medicineService.createMedicine(req.body);
+        res.status(201).json({
+            success: true,
+            message: "Medicine created successfully",
+            data: medicine
+        });
     }
-};
-
-// 4. DELETE medicine
-export const deleteMedicine = (req, res) => {
-    try{
-        const {id} = req.params;
-        const success = Medicine.delete(id);
-
-        if(!success) {
-            return res.status(400).json({message: "Medicine record not found."})
-        }
-        res.status(200).json('Deleted Successfully.');
-    } catch(error) {
-        res.status(500).json({error: error.message});
-    }
-};
+);
