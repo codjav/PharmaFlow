a470fa63a80eba29186b1b0b8dd75a3a90983cba
MD JAVED ALI <mdjavedali67990@gmail.com>
2026-07-02 00:34:29 +0530

    created main.jsx file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+import App from "./App";
+import { queryClient } from "./store/queryClient";
+
+import "./styles/globals.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+        <Toaster richColors position="top-right" closeButton />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
