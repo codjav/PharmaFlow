e8eebcc7f719c698e9893d8704ab7cb5e611d57a
MD JAVED ALI <mdjavedali67990@gmail.com>
2026-07-02 01:52:28 +0530

    Created navbar and layout for all pages

---
+++ frontend/src/components/layout/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <header className="flex h-20 items-center justify-between border-b bg-white px-8">
+        <h2 className="text-2xl font-semibold">
+            Dashboard
+        </h2>
+        <div>
+            Admin
+        </div>
+    </header>
+  );
+};
+
+export default Navbar;
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,10 +1,20 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import DashboardLayout from "../layouts/DashboardLayout";
+
+import LoginPage from "../pages/Login/LoginPage";
+import DashboardPage from "../pages/Dashboard/DashboardPage";
 
 function AppRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<div>Login</div>} />
-            <Route path="/dashboard" element={<div>Dashboard</div>} />
+            <Route path="/" element={<LoginPage />} />
+
+            <Route element={<DashboardLayout />}>
+                <Route path="/dashboard" element={<DashboardPage />} />
+            </Route>
+            
+            <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
     )
 }
