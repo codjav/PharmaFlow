63b0a89677e7ff54fc44050f3e4ddb3ad5ebd98d
MD JAVED ALI <mdjavedali67990@gmail.com>
2026-07-02 12:55:53 +0530

    Created spinner component

---
+++ frontend/src/components/ui/Spinner/Spinner.jsx
@@ -0,0 +1,19 @@
+import { Loader2 } from "lucide-react";
+import { cn } from "@/lib/cn";
+
+const Spinner = ({
+    className,
+    size = {20}
+}) => {
+    return (
+        <Loader2
+            size={size}
+            className={cn(
+                "animate-spin",
+                className
+            )}
+        />
+    )
+}
+
+export default Spinner;
