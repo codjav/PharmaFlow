0bb6a8c8d2e0e0a248bcc34a7fe4b9f958ad4da1
MD JAVED ALI <mdjavedali67990@gmail.com>
2026-07-02 11:34:22 +0530

    Created navbar component

--- frontend/src/components/layout/Navbar/Navbar.jsx
+++ frontend/src/components/layout/Navbar/Navbar.jsx
@@ -1,11 +1,18 @@
+import GlobalSearch from "./GlobalSearch";
+import NotificationButton from "./NotificationButton";
+import PageHeader from "./PageHeader";
+import ProfileDropdown from "./ProfileDropdown";
+import ThemeToggle from "./ThemeToggle";
+
 const Navbar = () => {
   return (
     <header className="flex h-20 items-center justify-between border-b bg-white px-8">
-        <h2 className="text-2xl font-semibold">
-            Dashboard
-        </h2>
-        <div>
-            Admin
+        <PageHeader/>
+        <div className="flex items-center gap-4">
+          <GlobalSearch />
+          <NotificationButton />
+          <ThemeToggle />
+          <ProfileDropdown />
         </div>
     </header>
   );
